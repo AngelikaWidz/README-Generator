c5d9707e45f7f07c48d97eff1e92af4e3b189e84
AngelikaWidz <angelikawidz@yahoo.com>
2024-01-30 00:50:47 +0000

    Questions + readme gen func

--- index.js
+++ index.js
@@ -4,13 +4,73 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+inquirer
+.prompt([
+    // pass questions here
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Project Name?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe project?'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Install instructions?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Usage directions?'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose the project licence:',
+    choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None']
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:'
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter GitHub username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter Email address:'
+  }
+])
+.then((answers) => {
+    // user feedback to generate readme
+    const readmeContent = generateREADME(answers);
 
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
+    // function to write README file
+    // fs.writeFile( file, data, options, callback )
+    fs.writeFile('README.md', readmeContent, 'utf8', (err) => {
+      if (err) {
+        console.error('Error', err);
+      } else {
+        console.log('README.md generated');
+      }
+    });
+  })
+  .catch((err) => {
+    console.error('Error:', err);
+  });
 
 // function to initialize program
 function init() {
