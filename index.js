95e43af91ef99028929615d8b921af5c2f68d490
AngelikaWidz <angelikawidz@yahoo.com>
2024-01-30 01:14:30 +0000

    markdown link to question array

--- index.js
+++ index.js
@@ -4,9 +4,63 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
+inquirer
+.prompt([
+    // pass questions here
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Project Name?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe project?'
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Install instructions?'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Usage directions?'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose the project licence:',
+    choices: ['MIT', 'Apache-2.0', 'GPL-3.0', 'BSD-3-Clause', 'None']
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Provide contribution guidelines:'
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:'
+  },
+  {
+    type: 'input',
+    name: 'username',
+    message: 'Enter GitHub username:'
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter Email address:'
+  }
 
-];
+])
+.then((answers) => {
+    // user feedback to generate readme
+    // .then takes (answers) as arguments to use an reference in markdown gen 
+    const readmeContent = generateMarkdown(answers);
+
+});
 
 // function to write README file
 function writeToFile(fileName, data) {
